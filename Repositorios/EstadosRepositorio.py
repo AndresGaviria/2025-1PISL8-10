bcfd95e2fdec06b472aea04fdc9b0784eadd17c6
andres <anacleto115@hotmail.com>
2025-03-31 21:06:20 -0500

    Clase 31-03-2025 9:06

--- Repositorios/EstadosRepositorio.py
+++ Repositorios/EstadosRepositorio.py
@@ -4,33 +4,17 @@ from Utilidades import Configuracion;
 
 class EstadosRepositorio:
 
-	def ConexionBasica(self) -> None:
+	def Listar(self) -> None:
 		try:
 			conexion = pyodbc.connect(Configuracion.Configuracion.strConnection);
 
-			consulta: str = """SELECT * FROM estados""";
-			cursor = conexion.cursor();
-			cursor.execute(consulta);
-
-			for elemento in cursor:
-				print(elemento);
-
-			cursor.close();
-			conexion.close();
-		except Exception as ex:
-			print(str(ex));
-
-	def ConexionBasica2(self) -> None:
-		try:
-			conexion = pyodbc.connect(Configuracion.Configuracion.strConnection);
-
-			consulta: str = """SELECT * FROM estados""";
+			consulta: str = """{CALL proc_select_estados();}""";
 			cursor = conexion.cursor();
 			cursor.execute(consulta);
 
 			lista: list = [];
 			for elemento in cursor:
-				entidad: Estados = Estados();
+				entidad: Estados = Estados.Estados();
 				entidad.SetId(elemento[0]);
 				entidad.SetNombre(elemento[1]);
 				lista.append(entidad);
@@ -40,43 +24,5 @@ class EstadosRepositorio:
 
 			for estado in lista:
 				print(str(estado.GetId()) + ", " + estado.GetNombre());
-		except Exception as ex:
-			print(str(ex));
-
-	def ConexionBasica3(self) -> None:
-		try:
-			conexion = pyodbc.connect(Configuracion.Configuracion.strConnection);
-
-			consulta: str = """SELECT p.id,
-					p.cedula,
-					p.nombre,
-					p.estado,
-					p.fecha,
-					p.activo,
-					e.id,
-					e.nombre
-				FROM `personas` p INNER JOIN `estados` e ON p.estado = e.id;""";
-			cursor = conexion.cursor();
-			cursor.execute(consulta);
-
-			for elemento in cursor:
-				print(elemento);
-
-			cursor.close();
-			conexion.close();
-		except Exception as ex:
-			print(str(ex));
-
-	def ConexionBasica4(self) -> None:
-		try:
-			conexion = pyodbc.connect(Configuracion.Configuracion.strConnection);
-
-			consulta: str = """INSERT INTO `db_personas`.`estados` (`nombre`) VALUES ('Test');""";
-			cursor = conexion.cursor();
-			cursor.execute(consulta);
-			cursor.commit();
-
-			cursor.close();
-			conexion.close();
 		except Exception as ex:
 			print(str(ex));
