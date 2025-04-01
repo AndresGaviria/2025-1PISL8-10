e41ac110a270c0bdc981839f6720a9987cad611f
andres <anacleto115@hotmail.com>
2025-03-31 20:49:35 -0500

    Clase 31-03-2025 8:49

--- Repositorios/Repositorio.py
+++ Repositorios/Repositorio.py
@@ -21,53 +21,62 @@ class Repositorio:
 			print(str(ex));
 
 	def ConexionBasica2(self) -> None:
-		conexion = pyodbc.connect(Configuracion.Configuracion.strConnection);
+		try:
+			conexion = pyodbc.connect(Configuracion.Configuracion.strConnection);
 
-		consulta: str = """SELECT * FROM estados""";
-		cursor = conexion.cursor();
-		cursor.execute(consulta);
+			consulta: str = """SELECT * FROM estados""";
+			cursor = conexion.cursor();
+			cursor.execute(consulta);
 
-		lista: list = [];
-		for elemento in cursor:
-			entidad: Estados = Estados();
-			entidad.SetId(elemento[0]);
-			entidad.SetNombre(elemento[1]);
-			lista.append(entidad);
+			lista: list = [];
+			for elemento in cursor:
+				entidad: Estados = Estados();
+				entidad.SetId(elemento[0]);
+				entidad.SetNombre(elemento[1]);
+				lista.append(entidad);
 
-		cursor.close();
-		conexion.close();
+			cursor.close();
+			conexion.close();
 
-		for estado in lista:
-			print(str(estado.GetId()) + ", " + estado.GetNombre());
+			for estado in lista:
+				print(str(estado.GetId()) + ", " + estado.GetNombre());
+		except Exception as ex:
+			print(str(ex));
 
 	def ConexionBasica3(self) -> None:
-		conexion = pyodbc.connect(Configuracion.Configuracion.strConnection);
+		try:
+			conexion = pyodbc.connect(Configuracion.Configuracion.strConnection);
 
-		consulta: str = """SELECT p.id,
-				p.cedula,
-				p.nombre,
-				p.estado,
-				p.fecha,
-				p.activo,
-				e.id,
-				e.nombre
-			FROM `personas` p INNER JOIN `estados` e ON p.estado = e.id;""";
-		cursor = conexion.cursor();
-		cursor.execute(consulta);
+			consulta: str = """SELECT p.id,
+					p.cedula,
+					p.nombre,
+					p.estado,
+					p.fecha,
+					p.activo,
+					e.id,
+					e.nombre
+				FROM `personas` p INNER JOIN `estados` e ON p.estado = e.id;""";
+			cursor = conexion.cursor();
+			cursor.execute(consulta);
 
-		for elemento in cursor:
-			print(elemento);
+			for elemento in cursor:
+				print(elemento);
 
-		cursor.close();
-		conexion.close();
+			cursor.close();
+			conexion.close();
+		except Exception as ex:
+			print(str(ex));
 
 	def ConexionBasica4(self) -> None:
-		conexion = pyodbc.connect(Configuracion.Configuracion.strConnection);
+		try:
+			conexion = pyodbc.connect(Configuracion.Configuracion.strConnection);
 
-		consulta: str = """INSERT INTO `db_personas`.`estados` (`nombre`) VALUES ('Test');""";
-		cursor = conexion.cursor();
-		cursor.execute(consulta);
-		cursor.commit();
+			consulta: str = """INSERT INTO `db_personas`.`estados` (`nombre`) VALUES ('Test');""";
+			cursor = conexion.cursor();
+			cursor.execute(consulta);
+			cursor.commit();
 
-		cursor.close();
-		conexion.close();
+			cursor.close();
+			conexion.close();
+		except Exception as ex:
+			print(str(ex));
