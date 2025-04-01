6a43a697d7e07893f62391b75a0aee3157ebef98
andres <anacleto115@hotmail.com>
2025-03-31 20:34:36 -0500

    Clase 31-03-2025 8:34

---
+++ Repositorios/EstadosRepositorio.py
@@ -0,0 +1,70 @@
+import pyodbc;
+from Entidades import Estados;
+from Utilidades import Configuracion;
+
+class EstadosRepositorio:
+
+	def ConexionBasica(self) -> None:
+		conexion = pyodbc.connect(Configuracion.Configuracion.strConnection);
+
+		consulta: str = """SELECT * FROM estados""";
+		cursor = conexion.cursor();
+		cursor.execute(consulta);
+
+		for elemento in cursor:
+			print(elemento);
+
+		cursor.close();
+		conexion.close();
+
+	def ConexionBasica2(self) -> None:
+		conexion = pyodbc.connect(Configuracion.Configuracion.strConnection);
+
+		consulta: str = """SELECT * FROM estados""";
+		cursor = conexion.cursor();
+		cursor.execute(consulta);
+
+		lista: list = [];
+		for elemento in cursor:
+			entidad: Estados = Estados();
+			entidad.SetId(elemento[0]);
+			entidad.SetNombre(elemento[1]);
+			lista.append(entidad);
+
+		cursor.close();
+		conexion.close();
+
+		for estado in lista:
+			print(str(estado.GetId()) + ", " + estado.GetNombre());
+
+	def ConexionBasica3(self) -> None:
+		conexion = pyodbc.connect(Configuracion.Configuracion.strConnection);
+
+		consulta: str = """SELECT p.id,
+				p.cedula,
+				p.nombre,
+				p.estado,
+				p.fecha,
+				p.activo,
+				e.id,
+				e.nombre
+			FROM `personas` p INNER JOIN `estados` e ON p.estado = e.id;""";
+		cursor = conexion.cursor();
+		cursor.execute(consulta);
+
+		for elemento in cursor:
+			print(elemento);
+
+		cursor.close();
+		conexion.close();
+
+	def ConexionBasica4(self) -> None:
+		conexion = pyodbc.connect(self.strConnection);
+
+		consulta: str = """INSERT INTO `db_personas`.`estados` (`nombre`) VALUES ('Test');""";
+		cursor = conexion.cursor();
+		cursor.execute(consulta);
+		cursor.commit();
+
+		cursor.close();
+		conexion.close();
--- main.py
+++ main.py
@@ -2,86 +2,12 @@ import datetime;
 import decimal;
 import pyodbc;
 from Entidades import Estados;
-
-# REPOSITORIO
-class Repositorio:
-	strConnection: str = """
-		Driver={MySQL ODBC 9.0 Unicode Driver};
-		Server=localhost;
-		Database=db_personas;
-		PORT=3306;
-		user=user_ptyhon;
-		password=Clas3s1Nt2024_!""";
-
-	def ConexionBasica(self) -> None:
-		conexion = pyodbc.connect(self.strConnection);
-
-		consulta: str = """SELECT * FROM estados""";
-		cursor = conexion.cursor();
-		cursor.execute(consulta);
-
-		for elemento in cursor:
-			print(elemento);
-
-		cursor.close();
-		conexion.close();
-
-	def ConexionBasica2(self) -> None:
-		conexion = pyodbc.connect(self.strConnection);
-
-		consulta: str = """SELECT * FROM estados""";
-		cursor = conexion.cursor();
-		cursor.execute(consulta);
-
-		lista: list = [];
-		for elemento in cursor:
-			entidad: Estados = Estados();
-			entidad.SetId(elemento[0]);
-			entidad.SetNombre(elemento[1]);
-			lista.append(entidad);
-
-		cursor.close();
-		conexion.close();
-
-		for estado in lista:
-			print(str(estado.GetId()) + ", " + estado.GetNombre());
-
-	def ConexionBasica3(self) -> None:
-		conexion = pyodbc.connect(self.strConnection);
-
-		consulta: str = """SELECT p.id,
-				p.cedula,
-				p.nombre,
-				p.estado,
-				p.fecha,
-				p.activo,
-				e.id,
-				e.nombre
-			FROM `personas` p INNER JOIN `estados` e ON p.estado = e.id;""";
-		cursor = conexion.cursor();
-		cursor.execute(consulta);
-
-		for elemento in cursor:
-			print(elemento);
-
-		cursor.close();
-		conexion.close();
-
-	def ConexionBasica4(self) -> None:
-		conexion = pyodbc.connect(self.strConnection);
-
-		consulta: str = """INSERT INTO `db_personas`.`estados` (`nombre`) VALUES ('Test');""";
-		cursor = conexion.cursor();
-		cursor.execute(consulta);
-		cursor.commit();
-
-		cursor.close();
-		conexion.close();
+from Repositorios import Repositorio;
 
 estado = Estados.Estados();
 print(estado.GetNombre());
 
-repositorio = Repositorio();
+repositorio = Repositorio.Repositorio();
 repositorio.ConexionBasica();
 # repositorio.ConexionBasica2();
 # repositorio.ConexionBasica3();
