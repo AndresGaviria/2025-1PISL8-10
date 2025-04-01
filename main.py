b714ad464bb8172862700922bba6617b3defc2d0
andres <anacleto115@hotmail.com>
2025-03-31 20:24:39 -0500

    Clase 31-03-2025 8:24

--- main.py
+++ main.py
@@ -1,82 +1,7 @@
 import datetime;
 import decimal;
 import pyodbc;
-
-"""
-	varInteger: int = 253; # int(253);
-	varLong: int = 2350006657; # int(2350006657);
-	varFloat: float = 10.5; float(10.2);
-	varDecimal: decimal = 15.6;
-	varString: str = "Test"; # str("Valor 2");
-	varDate: datetime = datetime.datetime.now();
-	varBool: bool = True; # bool(True);
-"""
-
-# DTO, ENTIDAD...
-class Estados:
-	id: int = 0;
-
-	def GetId(self) -> int:
-		return self.id;
-	def SetId(self, value: int) -> None:
-		self.id = value;
-
-	nombre: str = None;
-
-	def GetNombre(self) -> str:
-		return self.nombre;
-	def SetNombre(self, value: str) -> None:
-		self.nombre = value;
-
-class Personas:
-	id: int = 0;
-
-	def GetId(self) -> int:
-		return self.id;
-	def SetId(self, value: int) -> None:
-		self.id = value;
-
-	cedula: str = None;
-
-	def GetCedula(self) -> str:
-		return self.cedula;
-	def SetCedula(self, value: str) -> None:
-		self.cedula = value;
-
-	nombre: str = None;
-
-	def GetNombre(self) -> str:
-		return self.nombre;
-	def SetNombre(self, value: str) -> None:
-		self.nombre = value;
-
-	estado: int = None;
-
-	def GetEstado(self) -> int:
-		return self.estado;
-	def SetEstado(self, value: int) -> None:
-		self.estado = value;
-
-	fecha: datetime = None;
-
-	def GetFecha(self) -> datetime:
-		return self.fecha;
-	def SetFecha(self, value: datetime) -> None:
-		self.fecha = value;
-
-	activo: bool = None;
-
-	def GetActivo(self) -> bool:
-		return self.activo;
-	def SetActivo(self, value: bool) -> None:
-		self.activo = value;
-
-	_estado: Estados = None;
-
-	def Get_Estado(self) -> Estados:
-		return self._estado;
-	def Set_Estado(self, value: Estados) -> None:
-		self._estado = value;
+from Entidades import Estados;
 
 # REPOSITORIO
 class Repositorio:
@@ -153,14 +78,14 @@ class Repositorio:
 		cursor.close();
 		conexion.close();
 
-estado = Estados();
+estado = Estados.Estados();
 print(estado.GetNombre());
 
 repositorio = Repositorio();
-# repositorio.ConexionBasica();
+repositorio.ConexionBasica();
 # repositorio.ConexionBasica2();
 # repositorio.ConexionBasica3();
-repositorio.ConexionBasica4();
+# repositorio.ConexionBasica4();
 
 """
 VERSION DE PYTHON
