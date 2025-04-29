93198712fdeb785780270ee13305afe3cde07e45
andres <anacleto115@hotmail.com>
2025-04-28 20:49:17 -0500

    Clase 28-04-2025 8:49

--- main1.py
+++ main1.py
@@ -1,3 +1,23 @@
+import hashlib;
+import binascii, os;
+from Crypto.Cipher import AES;
+
+class EncriptarMD5:
+    def Ejecutar(self) -> None:
+        valor = "Clase de integracion";
+        cifrado = self.Cifrar(valor);
+        print(cifrado);
+
+    def Cifrar(self, valor: str) -> str:
+        resultado = str(hashlib.md5(valor.encode('utf-8')).hexdigest());
+        return resultado;
+
+    def Decifrar(self, valor: str) -> str:
+        return None;
+
+
+encriptarMD5 = EncriptarMD5();
+encriptarMD5.Ejecutar();
 
 
 """
